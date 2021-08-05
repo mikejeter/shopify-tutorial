4ad9829db6f76691108161f14e7e5689dbaadf73
mikejeter <mjeter45@yahoo.com>
2021-08-05 15:20:57 -0400

    Added GIT, SASS, and gulp to watch for changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
-}
+const gulp = require('gulp');
+const themekit = require('@shopify/themekit');
+const sass = require('gulp-sass')(require('sass'));
+const clean = require('gulp-clean-css');
 
-exports.default = defaultTask
+gulp.task('sass', function() {
+    return gulp.src('scss/global.scss')
+    .pipe(sass())
+    .pipe(clean( {compatibility: 'ie11'} ))
+    .pipe(gulp.dest('assets'));
+
+});
+
+gulp.task('watch', function() {
+    gulp.watch('scss/*.scss', gulp.series('sass'));
+    themekit.command('watch',{
+        allowLive: true,
+        env: 'development'
+    });
+
+});
+
